844b3d7444652102b1e8c495eb128357bd9dc019
Bubbawubba-dev <wong.annie@gmail.com>
2026-05-13 10:26:03 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import plotly.graph_objects as go
-import requests
 from plotly.subplots import make_subplots
 from datetime import datetime
 
