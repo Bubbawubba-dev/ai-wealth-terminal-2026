8793e86d375e3bb47ba22c58e4522c46564c374d
Bubbawubba-dev <wong.annie@gmail.com>
2026-05-28 15:54:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ if not check_password():
 def get_base_universe():
     return [
         "ASTS", "ANET", "BZFD", "HUT", "FLEX", "VCYT", "MSFT", "IONQ", "ARM", "ZS", "APP", "NASA", 
-        "RKLB", "SNDK", "CYBR", "INTC", "CIFR", "BZFD", "HUT", "FLEX", "VCYT", "RDDT", "IONQ", "QUBT", "QBTS",
+        "RKLB", "SNDK", "CYBR", "INTC", "CIFR", "RDDT", "IONQ", "QUBT", "QBTS",
         "AVGO",  "MU", "STX", "LITE", "TE", "BE", "APLD", "CLSK", "CRWV", "KEEL", "CORZ", "WYFI", "IREN", "NBIS",
         "ENPH", "QCOM", "SMCI", "RGTI", "ASTC", "SHOP", "FJET", "NVDA", "SHAZ",
     ]
