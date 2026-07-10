07b98c1a5c615cef4d0a10bb9637093779478378
Bubbawubba-dev <wong.annie@gmail.com>
2026-07-10 13:22:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1662,8 +1662,16 @@ with tab_sentiment:
                 intraday_snap_single = fetch_intraday_snapshot([selected_ticker])
                 intraday_df_single = intraday_snap_single.get(selected_ticker, pd.DataFrame())
                 daily_tail_for_shock = ticker_df.tail(30).rename(columns=str.title)
-					ticker_shock_obj = compute_ticker_shock(intraday_df_single, daily_tail_for_shock)
-                	ticker_shock_score = ticker_shock_obj["shock_score"]
+		try:
+    			ticker_shock_obj = compute_ticker_shock(intraday_df_single, daily_tail_for_shock) or {}
+		except Exception:
+    			ticker_shock_obj = {}
+
+		ticker_shock_score = (
+    			ticker_shock_obj.get("shock_score", np.nan)
+    			if isinstance(ticker_shock_obj, dict)
+    			else np.nan
+		)
 
                 # TOP METRICS ROW
                 col1, col2, col3, col4 = st.columns(4)
